09263f182051a8dd230cef16a12265fc5e44e76f
duydoanmanh <doanmanhduy.yb0210@gmail.com>
2021-03-12 19:09:12 +0700

    add paths train and eval model

--- face_recognition/config.py
+++ face_recognition/config.py
@@ -1,20 +1,32 @@
 import torch 
+
+device = torch.device("cpu")
 # invironment training cuda:0 or cpu 
 # device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
-device = torch.device("cpu")
-data_root = '/home/minglee/Documents/aiProjects/RepoGithub/Dataset/VN-celeb' 
+
+
+data_root = '/home/duydm/Documents/dataset/data_facerec/VN_celeb' 
 #help = "The root folder of training set."
-evaluate_dataset_root = '/home/minglee/Documents/aiProjects/RepoGithub/F-Vision/face_recognition/data'
+
 backbone_type =  'MobileFaceNet'  
 # ['ir', 'ir_se'], 'mode should be ir or ir_se' ,[50, 100, 152], 'num_layers should be 50,100, or 152'
 # help = "Mobilefacenets, Resnet."  
 loss_type = 'ArcFace' 
 # help = "mv-softmax, arcface, npc-face."
+
 lr = 0.1 
 # help='The initial learning rate.'
-validations = ['lfw', 'cfp_ff', 'cfp_fp', 'agedb_30', 'calfw' ,'cplfw', 'vgg2_fp']
+
+
+dataset_type = ['LFW']
+#, 'CALFW' ,'CPLFW'
 validations = ['agedb_30']
 # name of evaluate dataset
+
+
+dataset_paths = '../F-Vision/face_recognition/data_conf.yaml'
+# path to data store all link dataset for evaluate model 
+
 evaluate_nrof_folds = 5 
 # k folds of evaluate dataset 
 evaluate_batch_size = 64 
@@ -23,7 +35,7 @@ distance_metric = 1
 # distance metric for evaluate arcFace model   0: Euclidian, 1:Cosine similarity distance
 evaluate_subtract_mean = False 
 # Subtract feature mean before calculating distance
-out_dir = '../face_recognition/trash/Output_models/history/weights' 
+out_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/weights' 
 # help = "The folder to save models."
 epoches = 4 
 # help = 'The training epoches.'
@@ -31,21 +43,21 @@ step = '10, 13, 16'
 # help = 'Step for lr.'
 print_freq = 2 
 # help = 'The print frequency for training state.'
-save_freq = 100 
+save_freq = 10 
 # help = 'The save frequency for training state.'
-evaluate_every_epoch = 1
+eval_by_batch_idx = 10          
 # help evaluate dataset 
-batch_size = 2
+batch_size = 4
 # help='The training batch size over all gpus.'
 momentum = 0.9 
 # help = 'The momentum for sgd.'
-log_dir = '../face_recognition/trash/Output_models/history/log' 
+log_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/log' 
 # help = 'The directory to save log.log'
 tensorboardx_logdir = 'tensorboard' 
 # help = 'The directory to save tensorboardx logs'
-pretrain_model_path = '../face_recognition/trash/Output_models/history/weights/Final_epoch_0.pt'
+pretrain_model = '/home/duydm/Documents/F-Vision/face_recognition/trash/mobilefacenet/Epoch_17.pt'
 # help = 'The path of pretrained model'
-resume = True
+reload_model = True
 # help = 'Whether to resume from a checkpoint.'
 num_class = 72778
 #number of class
@@ -64,14 +76,15 @@ model_parameter = {'ResNet':
                       'feat_dim': feat_dim, 
                       'out_h': 7, 
                       'out_w': 7},
-                  'MobileFaceNet': 
+
+                     'MobileFaceNet': 
                       {'feat_dim': feat_dim, 
                       'out_h': 7, 
                       'out_w': 7 }
                       } 
 # model type   
 
-loss_paramenter = {'ArcFace':
+loss_parameter = {'ArcFace':
                         {'feat_dim': feat_dim,
                         'num_class': num_class,
                         'margin_arc': 0.35,
