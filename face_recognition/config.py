969aaaf749cbd7de897be2d11a79544dd0651e6a
duydoanmanh <doanmanhduy.yb0210@gmail.com>
2021-04-02 19:44:50 +0700

    add config file

--- face_recognition/config.py
+++ face_recognition/config.py
@@ -3,20 +3,19 @@ import torch
 # _____________________________invironment training (cuda:0 , cpu)_________________________ 
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 
-#_____________data training ___________________________________
-data_root = '/home/duydm/Documents/dataset/data_facerec/VN_celeb' 
+
 #help = "The root folder of training set."
 
 #_________________ load backbone __________________ 
-backbone_type =  'MobileFaceNet'    # ['ir', 'ir_se'], 'mode should be ir or ir_se' ,[50, 100, 152], 'num_layers should be 50,100, or 152'
+backbone_type =  'ResNet'    # ['ir', 'ir_se'], 'mode should be ir or ir_se' ,[50, 100, 152], 'num_layers should be 50,100, or 152'
 loss_type = 'ArcFace'   # help = "Mobilefacenets, Resnet."   support for type loss "mv-softmax, arcface, npc-face."
 lr = 0.1  # help='The initial learning rate.'
 
 # ________________ training _____________________________
+data_root = '/home/duydm/Documents/dataset/data_facerec/VN_celeb' 
 evaluate_nrof_folds = 5 # k folds of evaluate dataset
 batch_size = 4 # help evaluate dataset 
 epoches = 4  # number of epoch for training 
-dataset_paths = '/home/duydm/Documents/F-Vision/face_recognition/data_conf.yaml' # path to data store all link dataset for evaluate model 
 
 step = '10, 13, 16'  # help = 'Step for schedule lr.'
 print_freq = 2  # help = 'The print frequency for training state.'
@@ -32,16 +31,17 @@ num_workers = 4 #number of workers
 momentum = 0.9  # help = 'The momentum for sgd.'
 
 # ___________________ evaluate dataset _______________________
-
+evaluate_batch_size = 8
+num_of_pair = 120
+root_eval_dataset = '/home/duydm/Documents/F-Vision/face_recognition/trash/VN_celeb'
 evaluate_batch_size = 4  # batch size of evaluate 
-dataset_type = ['LFW','CALFW' ,'CPLFW', 'CFP_FP', 'CFP_FF'] # name of evaluate dataset
 # dataset_type = ['CFP_FP', 'CFP_FF'] 
 
 # ______________________ work place output model _____________________________
 
 out_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/weights'  # help = "The place of folder to save models log history training"
 log_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/log'  # help = 'The directory to save log.log'
-pretrain_model = '/home/duydm/Documents/F-Vision/face_recognition/trash/mobilefacenet/Epoch_17.pt' # help = 'The path of pretrained model'
+pretrain_model = '/home/duydm/Documents/F-Vision/face_recognition/trash/resnet50ir/Epoch_17.pt' # help = 'The path of pretrained model'
 tensorboardx_logdir = 'tensorboard'  # help = 'The directory to save tensorboardx logs'
 
 
