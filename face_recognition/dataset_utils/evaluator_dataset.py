6cf065550e4b55c924ca64989b30f378b94cf53b
duydoanmanh <doanmanhduy.yb0210@gmail.com>
2021-04-14 12:32:10 +0700

    add module caculate consine similarity and build module test result

--- face_recognition/dataset_utils/evaluator_dataset.py
+++ face_recognition/dataset_utils/evaluator_dataset.py
@@ -7,7 +7,7 @@
 import os
 import sys
 import numpy as np
-    
+import math 
 class Evaluator(object):
     """Implementation of LFW test protocal.
     
@@ -16,7 +16,7 @@ class Evaluator(object):
         pair_list(list): the pair list given by PairsParser.
         feature_extractor(object): a feature extractor.
     """
-    def __init__(self, data_loader, pairs_parser_factory, feature_extractor):
+    def __init__(self,  data_loader, pairs_parser_factory, feature_extractor, distance_metric = 0 ):
         """Init Evaluator.
 
         Args:
@@ -29,11 +29,29 @@ class Evaluator(object):
         pairs_parser = pairs_parser_factory.get_parser()
         self.pair_list = pairs_parser.parse_pairs()
         self.feature_extractor = feature_extractor
+        self.distance_metric = distance_metric
 
     def eval(self, model):
         image_name2feature = self.feature_extractor.extract_online(model, self.data_loader)
-        mean_acc, mean_tpr, mean_fpr ,std = self.test_one_model(self.pair_list, image_name2feature)
-        return mean_acc, mean_tpr, mean_fpr ,std
+        mean_dis_false, mean_dis_true, mean_acc, mean_tpr, mean_fpr ,std, best_thres = self.test_one_model(self.pair_list, image_name2feature)
+        
+        return mean_dis_false, mean_dis_true, mean_acc, mean_tpr, mean_fpr ,std, best_thres
+
+    def distance(self, feat1, feat2, is_normalize=True):
+        if is_normalize:
+            feat1 = feat1 / np.linalg.norm(feat1)
+            feat2 = feat2 / np.linalg.norm(feat2)
+        
+        if self.distance_metric == 0: 
+            # euclidian distance
+            diff = np.subtract(feat1,feat2)
+            cur_score = math.sqrt(np.sum(np.square(diff),0))
+        elif self.distance_metric==1: 
+            # Distance based on cosine similarity
+            cur_score = np.dot(feat1,feat2)
+        else: 
+            raise 'Undefined distance metric %d' % self.distance_metric 
+        return cur_score
 
     def test_one_model(self, test_pair_list, image_name2feature, is_normalize = True):
         """Get the accuracy of a model.
@@ -47,61 +65,74 @@ class Evaluator(object):
             mean: estimated mean accuracy.
             std: standard error of the mean.
         """
-        subsets_score_list = np.zeros((10, 600), dtype = np.float32)
-        subsets_label_list = np.zeros((10, 600), dtype = np.int8)
+       
+        size = len(test_pair_list) 
+        subsets_score_list = np.zeros((size), dtype = np.float32)
+        subsets_label_list = np.zeros((size), dtype = np.int8)
+
         for index, cur_pair in enumerate(test_pair_list):
-            cur_subset = index // 600
-            cur_id = index % 600
             image_name1 = cur_pair[0]
             image_name2 = cur_pair[1]
             label = cur_pair[2]
-            subsets_label_list[cur_subset][cur_id] = label
+
             feat1 = image_name2feature[image_name1]
             feat2 = image_name2feature[image_name2]
-            if not is_normalize:
-                feat1 = feat1 / np.linalg.norm(feat1)
-                feat2 = feat2 / np.linalg.norm(feat2)
-            cur_score = np.dot(feat1, feat2)
-            subsets_score_list[cur_subset][cur_id] = cur_score
 
-        subset_train = np.array([True] * 10)
+            subsets_label_list[index] = label
+            subsets_score_list[index] = self.distance(feat1, feat2)
+
+        train_score_list =  subsets_score_list
+        train_label_list =  subsets_label_list
+
+        mean_predict_label_false = np.mean(train_score_list[train_label_list==1])
+        mean_predict_labe_true  = np.mean(train_score_list[train_label_list==0])                           
+
         accu_list = []
         tpr_list = [] 
         fpr_list = []
-        for subset_idx in range(10):
-            test_score_list = subsets_score_list[subset_idx]
-            test_label_list = subsets_label_list[subset_idx]
-            subset_train[subset_idx] = False
-            train_score_list = subsets_score_list[subset_train].flatten()
-            train_label_list = subsets_label_list[subset_train].flatten()
-            subset_train[subset_idx] = True
-            best_thres = self.getThreshold(train_score_list, train_label_list)
 
-            positive_score_list = test_score_list[test_label_list == 1]
-            negtive_score_list = test_score_list[test_label_list == 0]
+        best_thres = self.getThreshold(train_score_list, train_label_list)
+        positive_score_list = train_score_list[train_label_list == 1]
+        negtive_score_list  = train_score_list[train_label_list == 0]
 
+        if not self.distance_metric:       
+            true_pos_pairs = np.sum(positive_score_list < best_thres)
+            true_neg_pairs = np.sum(negtive_score_list > best_thres)
+            false_neg_pairs = np.sum(positive_score_list > best_thres) 
+            false_pos_pairs = np.sum(negtive_score_list < best_thres)  
+
+            print('TP:{} TN:{} FN:{} FP:{}'.format(true_pos_pairs,true_neg_pairs,false_neg_pairs, false_pos_pairs))
+            tpr_list.append( true_pos_pairs/(np.sum(positive_score_list)))
+            fpr_list.append( false_pos_pairs/(np.sum(negtive_score_list)))
+            accu_list.append((true_pos_pairs + true_neg_pairs)/train_score_list.shape[0])
+
+            mean_acc = np.mean(accu_list)
+            mean_tpr = np.mean(tpr_list)
+            mean_fpr = np.mean(fpr_list)
+        
+        else: 
+            """
+            cosin in range(0,1). With one is simility of two emb. and zero is not. 
+            """
             true_pos_pairs = np.sum(positive_score_list > best_thres)
             true_neg_pairs = np.sum(negtive_score_list < best_thres)
             false_neg_pairs = np.sum(positive_score_list < best_thres) 
-            false_pos_pairs = np.sum(test_score_list > best_thres)  
-            
-            # tpr = true_pos_pairs/(true_pos_pairs + false_neg_pairs) 
-            # tnr = true_neg_pairs/(false_pos_pairs + true_neg_pairs)
-            # fnr = false_neg_pairs/(true_pos_pairs + false_neg_pairs) 
-            # fpr = false_pos_pairs/(false_pos_pairs + true_neg_pairs)
-
+            false_pos_pairs = np.sum(negtive_score_list > best_thres) 
+        
+            print('TP:{} TN:{} FN:{} FP:{}'.format(true_pos_pairs,true_neg_pairs,false_neg_pairs, false_pos_pairs))
             tpr_list.append( true_pos_pairs/(true_pos_pairs + false_neg_pairs) )
             fpr_list.append(false_pos_pairs/(false_pos_pairs + true_neg_pairs))
-            accu_list.append((true_pos_pairs + true_neg_pairs) / 600)
-
-        mean_acc = np.mean(accu_list)
-        mean_tpr = np.mean(tpr_list)
-        mean_fpr = np.mean(fpr_list)
+            accu_list.append((true_pos_pairs + true_neg_pairs) / train_score_list.shape[0])
+        
+            mean_acc = np.mean(accu_list)
+            mean_tpr = np.mean(tpr_list)
+            mean_fpr = np.mean(fpr_list)
+        
         std = np.std(accu_list, ddof=1) / np.sqrt(10) #ddof=1, division 9.
         
-        return mean_acc, mean_tpr, mean_fpr ,std
+        return mean_predict_label_false, mean_predict_labe_true, mean_acc, mean_tpr, mean_fpr ,std, best_thres
 
-    def getThreshold(self, score_list, label_list, num_thresholds=1000):
+    def getThreshold(self, score_list, label_list):
         """Get the best threshold by train_score_list and train_label_list.
         Args:
             score_list(ndarray): the score list of all pairs.
@@ -114,20 +145,44 @@ class Evaluator(object):
         neg_score_list = score_list[label_list == 0]
         pos_pair_nums = pos_score_list.size
         neg_pair_nums = neg_score_list.size
-        score_max = np.max(score_list)
-        score_min = np.min(score_list)
-        score_span = score_max - score_min
-        step = score_span / num_thresholds
-        threshold_list = score_min +  step * np.array(range(1, num_thresholds + 1)) 
-        fpr_list = []
-        tpr_list = []
-        for threshold in threshold_list:
-            fpr = np.sum(neg_score_list > threshold) / neg_pair_nums
-            tpr = np.sum(pos_score_list > threshold) /pos_pair_nums
-            fpr_list.append(fpr)
-            tpr_list.append(tpr)
-        fpr = np.array(fpr_list)
-        tpr = np.array(tpr_list)
-        best_index = np.argmax(tpr-fpr)
-        best_thres = threshold_list[best_index]
+
+        if self.distance_metric == 0: 
+            fpr_list = []
+            tpr_list = []
+            threshold_list = np.arange(0.5, 1.6, 0.1)
+            for threshold in threshold_list:
+                fpr = np.sum(neg_score_list < threshold) / neg_pair_nums
+                tpr = np.sum(pos_score_list < threshold) /pos_pair_nums
+
+                fpr_list.append(fpr)
+                tpr_list.append(tpr)
+            fpr = np.array(fpr_list)
+            tpr = np.array(tpr_list)
+            best_index = np.argmax(tpr - fpr)
+            best_thres = threshold_list[best_index]
+            """
+                TPR: Độ nhạy model còn được gọi là TPR(True positive rate) cho 
+                biết mức độ dự báo chính xác trong nhóm sự kiện positive.
+                FPR: Cho biết mức độ dự báo sai một sự kiện khi nó là negative nhưng kết luận là positive.
+                TPR - FPR:  toi thieu rui ro du doan sai. max TP va TN
+            """
+        else: 
+            num_thresholds = 1000
+            score_max = np.max(score_list)
+            score_min = np.min(score_list)
+            score_span = score_max - score_min
+            step = score_span / num_thresholds
+            threshold_list = score_min +  step * np.array(range(1, num_thresholds + 1)) 
+            fpr_list = []
+            tpr_list = []
+            for threshold in threshold_list:
+                fpr = np.sum(neg_score_list > threshold) / neg_pair_nums
+                tpr = np.sum(pos_score_list > threshold) /pos_pair_nums
+                fpr_list.append(fpr)
+                tpr_list.append(tpr)
+            fpr = np.array(fpr_list)
+            tpr = np.array(tpr_list)
+            best_index = np.argmax(tpr-fpr)
+            best_thres = threshold_list[best_index]
+        
         return  best_thres
--- face_recognition/dataset_utils/module_eval.py
+++ face_recognition/dataset_utils/module_eval.py
@@ -1,61 +1,135 @@
 import torch 
 import os 
 import numpy as np 
+import sys 
+sys.path.append('../F-Vision')
 from dataset_utils.train_dataset import CommonTestDataset 
 from dataset_utils.extractor_embedding import CommonExtractor 
 from dataset_utils.pairs_parser import PairsParserFactory
 from dataset_utils.evaluator_dataset import Evaluator 
 from torch.utils.data import DataLoader
-
-
+from pathlib import Path 
 import time
 import warnings
-import yaml 
 from prettytable import PrettyTable
+from backbone.backbone_def import BackboneFactory 
+from utils.model_loader import ModelLoader 
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
-device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+class GenPairImage():
+    def __init__(self, root_dir, text_file_path, text_label_path, num_of_pair ):
+        self.nofpair = int(num_of_pair) 
+        self.root_dir = root_dir 
+        self.text_file_path = text_file_path
+        self.text_label_path = text_label_path
+        # if not os.path.exists(text_file_path): 
+        print('generate pair pairse file \n')
+        self.num_of_pair_false()
+        self.num_of_pair_true()
+        # if not os.path.exists(text_label_path):
+        print('generate label file \n')
+        self.gen_label_file()
+
+    def gen_label_file(self): 
+        f = open(self.text_label_path,'w')
+        for sub_folder in os.listdir(self.root_dir):
+            if sub_folder.split('.')[-1] == 'txt': 
+                continue 
+            sub_list = os.listdir(os.path.join(self.root_dir,sub_folder))
+            for img in sub_list: 
+                line = str(sub_folder + '/' + img + '\n')
+                f.write(line)
+        f.close()
+        
+    def num_of_pair_false(self): 
+    
+        f = open(self.text_file_path, "w")
+
+        folder_img = os.listdir(self.root_dir)
+        sub_first = ""
+        maxx = 0
+        for item in folder_img: 
+            if item.split('.')[-1] !='txt': 
+                if len(os.listdir(self.root_dir + '/' + item)) > maxx: 
+                    maxx = len(os.listdir(self.root_dir + '/' + item)) 
+                    sub_first = item 
+                
+        cnt = 0
+        for item in folder_img: 
+            if item != sub_first and item.split('.')[-1] !='txt': 
+                folder_first  = os.listdir(self.root_dir + '/' + sub_first)
+                folder_second = os.listdir(self.root_dir + '/' + item)
+                for img_first in folder_first: 
+                    for img_second in folder_second: 
+                        f.write(str( sub_first + '/' + img_first) + " " + str( item + '/' + img_second) + " " + str(0) + "\n")
+                        cnt += 1
+                        if cnt >= int(self.nofpair//2): 
+                            return None
 
 
-class ModuleEval(): 
+    def num_of_pair_true(self): 
+
+        f = open(self.text_file_path, "a")
+        
+        folder_img = os.listdir(self.root_dir) 
+        cnt = 0 
+        for sub_folder in folder_img: 
+            if sub_folder.split('.')[-1] == 'txt':  
+                continue
+            sub_list = os.listdir(self.root_dir + '/' + sub_folder)
+            if len(sub_list) < 3: 
+                continue
+            for idxf, img_first in enumerate(sub_list): 
+                for idxs, img_second in enumerate(sub_list): 
+                    if idxs > idxf: 
+                        f.write(str(sub_folder +'/'+img_first) + " " + str(sub_folder + '/' + img_second) + " " + str(1) + "\n")
+                        cnt +=1 
+                        if cnt >= int(self.nofpair//2): 
+                            return None
+
+
+
+class ModuleEval():  
     """
     """ 
-    def __init__(self, dataset_conf_yaml, model, batch_size, dataset_type): 
+    def __init__(self, data_path, model, conf, gen_pair=True, status='eval'): 
         self.model = model 
         if torch.cuda.is_available(): 
             model = torch.nn.DataParallel(self.model).cuda()
-        print('data path', dataset_conf_yaml)
-        self.dataset_conf_yaml = dataset_conf_yaml
-        self.batch_size = batch_size
-        self.dataset_type = dataset_type
+        self.conf = conf
+        self.data_path = data_path
+        self.text_file_path = self.data_path + '/' + 'pairs_file_path.txt'
+        self.text_label_path = self.data_path + '/' + 'image_list_file_path.txt'
+        self.distance_metric = conf.distance_metric
+        self.result_test_file = conf.result_test_file
+        if gen_pair:
+            GenPairImage(self.data_path, self.text_file_path, self.text_label_path, conf.num_of_pair)
+
 
     def eval(self): 
-        inf_list = []
-        feature_extractor = CommonExtractor(device,type='eval') 
+        f = open(self.result_test_file, "a") 
 
-        with open(self.dataset_conf_yaml) as f: 
-            dataset_paths = yaml.load(f) 
+        feature_extractor = CommonExtractor(self.conf.device) 
+        print("load eval dataset ! \n")    
+        t = time.time()
+        pairs_file =            self.text_file_path
+        cropped_foler_img =     self.data_path
+        image_list_label_path = self.text_label_path 
+        pairs_parser_factory = PairsParserFactory(pairs_file)
 
-        for data_type in self.dataset_type: 
-            print('\nload eval dataset {} done !\n'.format(data_type))
-            t = time.time()
-            pairs_file =            dataset_paths[data_type]['pairs_file_path']
-            cropped_foler_img =     dataset_paths[data_type]['cropped_face_folder']
-            image_list_label_path = dataset_paths[data_type]['image_list_file_path'] 
-
-            pairs_parser_factory = PairsParserFactory(pairs_file, data_type)
-            data_loader = DataLoader(CommonTestDataset(cropped_foler_img, image_list_label_path, False),
-                            batch_size=self.batch_size, num_workers = 4, shuffle=False)
-            feature_extractor = CommonExtractor(device, type='eval')
-            evaluator_dataset = Evaluator(data_loader, pairs_parser_factory, feature_extractor) 
-            mean_acc, mean_tpr, mean_fpr ,std = evaluator_dataset.eval(self.model)
-
-            inf_list.append((mean_acc, mean_tpr, mean_fpr, std, time.time()-t, data_type))
-            
-
-        pretty_tabel = PrettyTable(["mean accuracy","mean tpr","mean fpr" , "standard error", "time processing", "dataset type"])
-        for row in inf_list:
-            pretty_tabel.add_row(row)
-        print(pretty_tabel)       
-
+        data_loader = DataLoader(CommonTestDataset(cropped_foler_img, image_list_label_path, False),batch_size= self.conf.evaluate_batch_size, num_workers = self.conf.num_workers, shuffle=False)
+        
+        feature_extractor = CommonExtractor(self.conf.device)
+        
+        evaluator_dataset = Evaluator(data_loader = data_loader, pairs_parser_factory =pairs_parser_factory , feature_extractor = feature_extractor, distance_metric = self.distance_metric) 
+        mean_dis_false, mean_dis_true, mean_acc, mean_tpr, mean_fpr ,_, best_thres = evaluator_dataset.eval(self.model)
+        
+        metric_eval = "mean consine of pairs label" if self.distance_metric ==1 else "mean distance of pairs label"
+        
+        pretty_tabel = PrettyTable([metric_eval + ' false', metric_eval + ' true', "mean accuracy", "mean tpr", "mean fpr" , "time processing", "best threshold", "backbone type" ])
+        pretty_tabel.add_row((mean_dis_false,  mean_dis_true,    mean_acc,    mean_tpr,    mean_fpr , time.time() - t ,best_thres, self.conf.backbone_type ))
+        for row in pretty_tabel: 
+            f.write(row.get_string())
+        f.close()
