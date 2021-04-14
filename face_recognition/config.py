cb36a9b2ecac3fcf2387b1bbeab3997d09e7b8f4
duydoanmanh <doanmanhduy.yb0210@gmail.com>
2021-04-14 12:30:31 +0700

    change path and ignore file result.txt

--- face_recognition/config.py
+++ face_recognition/config.py
@@ -3,20 +3,21 @@ import torch
 # _____________________________invironment training (cuda:0 , cpu)_________________________ 
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 
-#_____________data training ___________________________________
-data_root = '/home/duydm/Documents/dataset/data_facerec/VN_celeb' 
-#help = "The root folder of training set."
+distance_metric = 0 # 0 is euclidian distance \ 1 is consin similarity. 
 
+#help = "The root folder of training set."
+result_test_file = '/home/duydm/CodeProject/F-Vision/face_recognition/result.txt'
 #_________________ load backbone __________________ 
-backbone_type =  'MobileFaceNet'    # ['ir', 'ir_se'], 'mode should be ir or ir_se' ,[50, 100, 152], 'num_layers should be 50,100, or 152'
+#backbone_type =  'ResNet'    # ['ir', 'ir_se'], 'mode should be ir or ir_se' ,[50, 100, 152], 'num_layers should be 50,100, or 152'
+backbone_type = 'MobileFaceNet'
 loss_type = 'ArcFace'   # help = "Mobilefacenets, Resnet."   support for type loss "mv-softmax, arcface, npc-face."
 lr = 0.1  # help='The initial learning rate.'
 
 # ________________ training _____________________________
+data_root = ''  # path of data training
 evaluate_nrof_folds = 5 # k folds of evaluate dataset
 batch_size = 4 # help evaluate dataset 
 epoches = 4  # number of epoch for training 
-dataset_paths = '/home/duydm/Documents/F-Vision/face_recognition/data_conf.yaml' # path to data store all link dataset for evaluate model 
 
 step = '10, 13, 16'  # help = 'Step for schedule lr.'
 print_freq = 2  # help = 'The print frequency for training state.'
@@ -32,25 +33,32 @@ num_workers = 4 #number of workers
 momentum = 0.9  # help = 'The momentum for sgd.'
 
 # ___________________ evaluate dataset _______________________
-
-evaluate_batch_size = 4  # batch size of evaluate 
-dataset_type = ['LFW','CALFW' ,'CPLFW', 'CFP_FP', 'CFP_FF'] # name of evaluate dataset
-# dataset_type = ['CFP_FP', 'CFP_FF'] 
+num_of_pair = 120000
+root_eval_dataset = '/home/duydm/CodeProject/F-Vision/face_recognition/trash/face-with-mask-VN-celeb'  # data with mask
+#root_eval_dataset = '/home/duydm/CodeProject/F-Vision/face_recognition/trash/VN_celeb'  # data without mask 
+evaluate_batch_size = 64  # batch size of evaluate
 
 # ______________________ work place output model _____________________________
 
-out_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/weights'  # help = "The place of folder to save models log history training"
-log_dir = '/home/duydm/Documents/F-Vision/face_recognition/trash/Output_models/history/log'  # help = 'The directory to save log.log'
-pretrain_model = '/home/duydm/Documents/F-Vision/face_recognition/trash/mobilefacenet/Epoch_17.pt' # help = 'The path of pretrained model'
-tensorboardx_logdir = 'tensorboard'  # help = 'The directory to save tensorboardx logs'
+out_dir = 'Output_models'  # help = "The place of folder to save models log history training"
+log_dir = 'Output_models/history/log'  # help = 'The directory to save log.log'
+
+#pretrain_model = '/home/duydm/CodeProject/F_Vision/F-Vision/face_recognition/trash/ResNeSt50/Epoch_17.pt' # ResNeSt50 
+#pretrain_model= '/home/duydm/CodeProject/F-Vision/face_recognition/trash/mobilefacenet/Epoch_17.pt' # mobilefacenet
+#pretrain_model = '/home/duydm/CodeProject/F-Vision/face_recognition/trash/resnet50ir/Epoch_17.pt' #  resnetir 50
+pretrain_model = '/home/duydm/CodeProject/F-Vision/face_recognition/trash/resnet152-ir_se/Epoch_17.pt' # resnet ir-se 152
+
+
+ # help = 'The path of pretrained model'
+tensorboardx_logdir = 'Output_models/tensorboard'  # help = 'The directory to save tensorboardx logs'
 
 
 # ______________________ define parametor of backbone and loss type __________________________________
 
 model_parameter = {'ResNet': 
-                      {'depth': 50,   # 50,100, or 152'
+                      {'depth': 152,   # 50,100, or 152'
                       'drop_ratio': 0.4, 
-                      'net_mode': 'ir',  # ['ir', 'ir_se']
+                      'net_mode': 'ir_se',  # ['ir', 'ir_se']
                       'feat_dim': feat_dim, 
                       'out_h': 7, 
                       'out_w': 7},
